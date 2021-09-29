7b96dc8064dbcc4229f74d28dfb529cb5c160cb2
Giriprasath <giriarjun1995@gmail.com>
2021-09-28 21:55:04 -0600

    styling done

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 outerdiv = document.createElement('div');
 document.body.appendChild(outerdiv);
 
+
+
+data_count=1
+
     
     //fetching the api data
 var url='https://api.openbrewerydb.org/breweries'
@@ -24,7 +28,9 @@ Validate = (table) => {
         
         if (cells[i].innerHTML === '' || cells[i].innerHTML === 'null')
 
-            console.log(cells[i].innerHTML = 'No Data');
+            cells[i].innerHTML = 'No Data';
+            
+
     }
 
    
@@ -35,23 +41,27 @@ setData = (brewdata) => {
     brewdata.map((breweries) => {
         
         
-        table = document.createElement('table')
+        table = document.createElement('table');
+        
     
         name_row = table.insertRow();
         cell_1 = name_row.insertCell();
         cell_1.innerHTML = 'Name';
+        cell_1.setAttribute('class','lables')
         cell_2 = name_row.insertCell();
         cell_2.innerHTML = breweries.name;
 
         type_row = table.insertRow();
         cell_1 = type_row.insertCell();
         cell_1.innerHTML = 'Type';
+        cell_1.setAttribute('class','lables')
         cell_2 = type_row.insertCell();
         cell_2.innerHTML = breweries.brewery_type;
 
         address_row = table.insertRow();
         cell_1 = address_row.insertCell();
         cell_1.innerHTML = 'Address';
+        cell_1.setAttribute('class','lables')
         cell_2 = address_row.insertCell();
         if (breweries.street===null) {
             cell_2.innerHTML = `${breweries.city},${breweries.state}-(${breweries.postal_code})`;
@@ -63,23 +73,31 @@ setData = (brewdata) => {
         website_row = table.insertRow();
         cell_1 = website_row.insertCell();
         cell_1.innerHTML = 'Website';
+        cell_1.setAttribute('class','lables')
         cell_2 = website_row.insertCell();
         if (breweries.website_url===null) {
             cell_2.innerHTML = `No Data`;
         } else {
-            cell_2.innerHTML = `<a href='${breweries.website_url}' target="_blank">Click Here</a>`;
+            cell_2.innerHTML = `<a href='${breweries.website_url}' target="_blank">${breweries.website_url}</a>`;
         }
         
 
         phone_row = table.insertRow();
         cell_1 = phone_row.insertCell();
         cell_1.innerHTML = 'Phone Number';
+        cell_1.setAttribute('class','lables')
         cell_2 = phone_row.insertCell();
         cell_2.innerHTML = breweries.phone;
 
-        outerdiv.appendChild(table);
-
+        heading = document.createElement('h3');
+        heading.innerHTML = `Data-${data_count}`
+        
         Validate(table);
+
+        outerdiv.appendChild(heading);
+        outerdiv.appendChild(table);
+        data_count++
+
 
         br = document.createElement('br')
         outerdiv.appendChild(br)
