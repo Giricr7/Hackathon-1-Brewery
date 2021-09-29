edb2153f909709cfeca3429f464a03386ae4c6d1
Giriprasath <giriarjun1995@gmail.com>
2021-09-29 07:50:12 -0600

    added comments

--- script.js
+++ script.js
@@ -3,8 +3,6 @@
 outerdiv = document.createElement('div');
 document.body.appendChild(outerdiv);
 
-
-
 data_count=1
 
     
@@ -19,7 +17,7 @@ fetchData = async () => {
     return convertedData
 }
 
-
+// removing the empty values from the data
 Validate = (table) => {
     
     cells=table.querySelectorAll('td')
@@ -36,19 +34,21 @@ Validate = (table) => {
    
 }
 
+
+//creating and inserting tables and data
 setData = (brewdata) => {
    
+    //creating header for the page
     h1 = document.createElement('h1');
     h1.innerHTML = 'Breweries List'
-    
     outerdiv.appendChild(h1)
 
     brewdata.map((breweries) => {
         
-        
+     //creating a table element   
         table = document.createElement('table');
         
-    
+    //inserting name from the data
         name_row = table.insertRow();
         cell_1 = name_row.insertCell();
         cell_1.innerHTML = 'Name';
@@ -56,6 +56,7 @@ setData = (brewdata) => {
         cell_2 = name_row.insertCell();
         cell_2.innerHTML = breweries.name;
 
+    //inserting type from the data
         type_row = table.insertRow();
         cell_1 = type_row.insertCell();
         cell_1.innerHTML = 'Type';
@@ -63,6 +64,7 @@ setData = (brewdata) => {
         cell_2 = type_row.insertCell();
         cell_2.innerHTML = breweries.brewery_type;
 
+    //inserting address from the data
         address_row = table.insertRow();
         cell_1 = address_row.insertCell();
         cell_1.innerHTML = 'Address';
@@ -74,7 +76,7 @@ setData = (brewdata) => {
             cell_2.innerHTML = `${breweries.street},${breweries.city},${breweries.state}-${breweries.postal_code}`;
         }
         
-
+    //inserting website url from the data
         website_row = table.insertRow();
         cell_1 = website_row.insertCell();
         cell_1.innerHTML = 'Website';
@@ -86,7 +88,7 @@ setData = (brewdata) => {
             cell_2.innerHTML = `<a href='${breweries.website_url}' target="_blank">${breweries.website_url}</a>`;
         }
         
-
+    //inserting phone number from the data
         phone_row = table.insertRow();
         cell_1 = phone_row.insertCell();
         cell_1.innerHTML = 'Phone Number';
@@ -94,16 +96,19 @@ setData = (brewdata) => {
         cell_2 = phone_row.insertCell();
         cell_2.innerHTML = breweries.phone;
 
+    //creating the sub-header for each table
         heading = document.createElement('h3');
         heading.innerHTML = `Data-${data_count}`
         
+    //calling the validating function
         Validate(table);
 
+    //inserting the sub-header and table into HTML page
         outerdiv.appendChild(heading);
         outerdiv.appendChild(table);
         data_count++
 
-
+    //creating spacing between each table
         br = document.createElement('br')
         outerdiv.appendChild(br)
         outerdiv.appendChild(br)
@@ -113,12 +118,13 @@ setData = (brewdata) => {
 
 }
 
+    // main function
     async function start () {
         let brewdata = await fetchData();
         setData(brewdata);
     
     }
 
-
+    //program execution starts here
 start();
 
